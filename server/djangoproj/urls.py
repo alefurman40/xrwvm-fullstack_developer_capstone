ed6ed7775eb586542b036394a5ac390c835b1d33
alefurman40 <alefurman@gmail.com>
2024-09-26 15:11:51 -0400

    Adding temporary changes to Github

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -31,5 +31,6 @@ urlpatterns = [
     path('get_dealers/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
