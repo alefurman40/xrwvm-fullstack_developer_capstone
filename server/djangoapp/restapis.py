b4749685804e7682a6fc6d61da191642c5d08734
alefurman40 <alefurman@gmail.com>
2024-10-01 13:35:53 -0400

    lint 12

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -14,14 +14,13 @@ sentiment_analyzer_url = os.getenv(
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 
-
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params + key + "=" + value + "&"
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint + "?" + params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
@@ -31,7 +30,7 @@ def get_request(endpoint, **kwargs):
         response = requests.get(request_url)
         print(response)
         return response.json()
-    except Exception as e: 
+    except Exception as e:
         print(e)
         # If any error occurs
         # print("Network exception occurred")
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,10 +3,10 @@
 from django.shortcuts import render
 # from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
+# from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
+# from django.contrib import messages
+# from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -21,6 +21,7 @@ from .restapis import get_request, analyze_review_sentiments, post_review
 logger = logging.getLogger(__name__)
 
 # Create your views here.
+
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
